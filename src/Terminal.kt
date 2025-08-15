99304e735bdd6dcf41d4e4b62683c2538b3a7c6b
AshtonHill <30095596@tafe.wa.edu.au>
2025-08-15 10:50:42 +0800

    Commit 2

--- src/Terminal.kt
+++ src/Terminal.kt
@@ -1,6 +1,19 @@
 class Terminal {
 
 
+    fun transfer(card1: Card, card2: Card, transferCreditAmount : Double, transferTicketAmount : Int) : String{
+        card1.creditBalance -= transferCreditAmount
+        card1.ticketBalance -= transferTicketAmount
+        card2.creditBalance += transferCreditAmount
+        card2.ticketBalance += transferTicketAmount
+        return "$$transferCreditAmount and $transferTicketAmount tickets were transferred from card ${card1.cardID} to card ${card2.cardID}"
+    }
+    fun display(card: Card): String {
+        return "Card ID: ${card.cardID}, Card Balance: ${card.creditBalance}, Card Tickets: ${card.ticketBalance}"
+    }
+    fun cashToCredit(cash: Int) : Int{
+        return cash * 2
+    }
     fun checkCardBalance(card: Card): Double{
         return card.creditBalance
     }
